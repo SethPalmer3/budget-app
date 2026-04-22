e3edc211f800c3fcaa92e24c33ab0ef467289988
SethPalmer3 <elderlord30@gmail.com>
2026-04-22 06:30:50 -0700

    Added date module.
    Didn't have time to make the `before` method be passed by reference.

---
+++ src/date.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+const Date = @This();
+
+day: u8,
+month: u8,
+year: u64,
+
+pub fn before(this: &Date, that: &Date) bool {
+    return this.year < that.year or this.month < that.month or this.day < that.day;
+}
+
+pub fn init(day: u8, month: u8, year: u64) Date {
+    return .{
+        .day = day,
+        .month = month,
+        .year = year,
+    };
+}
+
+test "before method check year" {
+    const this: Date = Date.init(1, 1, 2000);
+    const that: Date = Date.init(1, 1, 2100);
+    try std.testing.expect(this.before(that));
+}
+test "before method check month" {
+    const this: Date = Date.init(1, 1, 2000);
+    const that: Date = Date.init(1, 2, 2000);
+    try std.testing.expect(Date.before(this, that));
+}
+test "before method check day" {
+    const this: Date = Date.init(1, 1, 2000);
+    const that: Date = Date.init(2, 1, 2000);
+    try std.testing.expect(Date.before(this, that));
+}
