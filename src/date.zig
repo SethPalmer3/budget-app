4c52430cd063debd6e6bfa8e830c7b21290e28a5
SethPalmer3 <elderlord30@gmail.com>
2026-04-22 19:02:20 -0700

    First iteration of date and switching to transaction managers

--- src/date.zig
+++ src/date.zig
@@ -5,7 +5,7 @@ day: u8,
 month: u8,
 year: u64,
 
-pub fn before(this: &Date, that: &Date) bool {
+pub fn before(this: *const Date, that: *const Date) bool {
     return this.year < that.year or this.month < that.month or this.day < that.day;
 }
 
@@ -20,15 +20,15 @@ pub fn init(day: u8, month: u8, year: u64) Date {
 test "before method check year" {
     const this: Date = Date.init(1, 1, 2000);
     const that: Date = Date.init(1, 1, 2100);
-    try std.testing.expect(this.before(that));
+    try std.testing.expect(Date.before(&this, &that));
 }
 test "before method check month" {
     const this: Date = Date.init(1, 1, 2000);
     const that: Date = Date.init(1, 2, 2000);
-    try std.testing.expect(Date.before(this, that));
+    try std.testing.expect(Date.before(&this, &that));
 }
 test "before method check day" {
     const this: Date = Date.init(1, 1, 2000);
     const that: Date = Date.init(2, 1, 2000);
-    try std.testing.expect(Date.before(this, that));
+    try std.testing.expect(Date.before(&this, &that));
 }
--- src/datums.zig
+++ src/datums.zig
@@ -1,6 +1,9 @@
 /// File containing the datum structures that the
 /// budget and transacation will be based around
 /// including their "types"
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const Date = @import("./date.zig");
 const Datums = @This();
 
 pub const Type = enum {
@@ -11,12 +14,22 @@ pub const Type = enum {
     Saving,
 };
 
-pub fn Budget(comptime group_size: u8) type {
+pub const BudgetManagerConfig = struct {
+    member_size: u8,
+    description_length: usize,
+    name_length: usize,
+    database_writer: std.Io.Writer,
+    database_reader: std.Io.Reader,
+};
+
+pub fn BudgetManager(comptime config: BudgetManagerConfig) type {
     return struct {
-        budget_type: Datums.Type,
-        who: [group_size][]u8, // Array of strings for names
-        amoutn: u64, // Using integer for fixed point numbers
-        description: []u8,
-        //TODO: Implement date objects if zig doesn't have one
+        member_size: u8 = config.member_size,
+        description_length: usize = config.description_length,
+        name_length: usize = config.name_length,
+        database_writer: std.Io.Writer = config.writer,
+        database_reader: std.Io.Reader = config.reader,
+
+        const Self = @This();
     };
 }
