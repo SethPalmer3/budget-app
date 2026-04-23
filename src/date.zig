2114e51be32603df13b9d321814c979ab57217bf
SethPalmer3 <elderlord30@gmail.com>
2026-04-23 06:31:37 -0700

    Adding strings to dates and finding good implementations for budgeters
    The date structure now allows to output a date in slice form. Next needs
    to add a way to convert a string/slice into a date structure.

--- src/date.zig
+++ src/date.zig
@@ -1,12 +1,21 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const Date = @This();
 
 day: u8,
 month: u8,
 year: u64,
 
-pub fn before(this: *const Date, that: *const Date) bool {
+pub inline fn before(this: *const Date, that: *const Date) bool {
     return this.year < that.year or this.month < that.month or this.day < that.day;
+}
+
+pub fn after(this: *const Date, that: *const Date) bool {
+    return !Date.before(this, that) and !Date.same(this, that);
+}
+
+pub inline fn same(this: *const Date, that: *const Date) bool {
+    return this.year == that.year and this.month == that.month and this.day == that.day;
 }
 
 pub fn init(day: u8, month: u8, year: u64) Date {
@@ -15,6 +24,13 @@ pub fn init(day: u8, month: u8, year: u64) Date {
         .month = month,
         .year = year,
     };
+}
+
+pub fn stringify(date: *const Date, gpa: Allocator) []u8 {
+    var date_str_buffer: []u8 = gpa.alloc(u8, 3 + 3 + 4 + 1);
+    std.fmt.allocPrint(gpa, "{:0>2}/{:0>2}/{:0>4}", .{ date.month, date.day, date.year });
+
+    return date_str_buffer[0..];
 }
 
 test "before method check year" {
--- src/datums.zig
+++ src/datums.zig
@@ -37,6 +37,16 @@ pub fn BudgeterItem(comptime config: BudgeterManagerConfig) type {
         date: Date,
     };
 }
+pub fn QueryBudgeterItem(comptime config: BudgeterManagerConfig) type {
+    return struct {
+        budgeter_type: ?Datums.BudgeterType,
+        tranaction_type: ?Datums.TransactionType,
+        who: ?[config.member_size][config.name_length]u8,
+        amount: ?u64, // Will treat as a fix point value
+        description: ?[config.description_length]u8,
+        date: ?Date,
+    };
+}
 
 pub fn BudgeterManager(comptime config: BudgeterManagerConfig) type {
     return struct {
@@ -50,5 +60,6 @@ pub fn BudgeterManager(comptime config: BudgeterManagerConfig) type {
         pub fn readBudgetItem(bm: *Self, identifier: anytype) !BudgeterItem(config) { // Not sure yet what to use as a identifier
             // TODO: Implement
         }
+        pub fn filterBudgetItems(bm: *Self, query: QueryBudgeterItem(config)) ![]BudgeterItem(config) {}
     };
 }
