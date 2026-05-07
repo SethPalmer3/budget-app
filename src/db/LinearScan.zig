632b019640e886e3b7c0630a41ae7eb6ad1ca5ff
SethPalmer3 <elderlord30@gmail.com>
2026-05-06 18:31:10 -0700

    Setup data dir permissions and restructured buffers
    Finally found how to give the created directories the correct
    permissions and allocated the buffer from initalization to hopefully
    reduce complexity. Also found a better way to create and open
    directories without knowing for sure if it's an absolute path or a
    relative one.

--- src/db/LinearScan.zig
+++ src/db/LinearScan.zig
@@ -1,43 +1,117 @@
 const std = @import("std");
 const Database = @import("./database.zig");
 
-pub const Options = struct {
-    data_directory: []const u8 = "./",
-    heap_file_name: []const u8 = "heap.db",
-    io: std.Io = undefined,
-    buffer_size: usize = 1024,
+const Options = struct {
+    data_path: []const u8,
+    heap_file_name: []const u8,
+    io: std.Io,
+    buffer_size: usize,
 };
 
-pub fn LinearStorageEngine(comptime options: Options) type {
+/// Opens the `path` directory first by trying to open using
+/// and absolute path then checks the current working directory
+/// if the absolute path fails
+inline fn open_dir_abs_or_cwd(io: std.Io, path: []const u8) !std.Io.Dir {
+    if (std.fs.path.isAbsolute(path)) {
+        return try std.Io.Dir.openDirAbsolute(io, path, .{});
+    }
+    return std.Io.Dir.cwd().openDir(io, path, .{});
+}
+
+inline fn create_dir_abs_or_cwd(io: std.Io, path: []const u8) !void {
+    if (std.fs.path.isAbsolute(path)) {
+        try std.Io.Dir.createDirAbsolute(
+            io,
+            path,
+            .default_dir,
+        );
+        return;
+    }
+    try std.Io.Dir.cwd().createDirPath(io, path); // Not sure why createDirPath works but createPath doesn't
+}
+
+pub fn LinearStorageEngine(comptime RecorcType: type, comptime KeyType: type) type {
     return struct {
-        pub fn addData(key: []const u8, data: []const u8) !void {
-            const current_dir = try std.Io.Dir.cwd().openDir(options.io, options.data_directory, .{});
-            const storage_file = try current_dir.createFile(options.io, options.heap_file_name, .{
-                .truncate = false,
-            });
-            defer storage_file.close(options.io);
-            const storage_file_stat = try storage_file.stat(options.io);
-            const storage_file_size = storage_file_stat.size;
-            var buff: [options.buffer_size]u8 = undefined;
-            var storage_file_writer = storage_file.writer(options.io, &buff);
-            var storage_writer = storage_file_writer.interface;
-            try storage_file_writer.seekTo(storage_file_size);
-            _ = try storage_writer.write(key);
-            _ = try storage_writer.write(data);
+        options: Options,
+        buff: []u8,
+        const Self = @This();
+
+        /// Initalize the generic type and allocate the buffer
+        pub fn init(gpa: std.mem.Allocator, options: Options) !Self {
+            const io = options.io;
+            try create_dir_abs_or_cwd(io, options.data_path);
+            return .{
+                .buff = try gpa.alloc(u8, options.buffer_size),
+                .options = options,
+            };
         }
+
+        /// Free up the allocated buffer
+        pub fn deinit(lse: *Self, gpa: std.mem.Allocator) void {
+            gpa.free(lse.buff);
+        }
+
+        pub fn storeData(lse: *Self, key: KeyType, record: RecorcType) !void {
+            const io = lse.options.io;
+            const data_directory = try open_dir_abs_or_cwd(io, lse.options.data_path);
+            const heap_file = try data_directory.createFile(io, lse.options.heap_file_name, .{ .truncate = false });
+            defer heap_file.close(io);
+            const heap_file_stat = try heap_file.stat(io);
+
+            var heap_file_writer = heap_file.writer(io, lse.buff);
+            try heap_file_writer.seekTo(heap_file_stat.size); // Go to the end of file
+            var generic_writer = &heap_file_writer.interface;
+
+            // Write data
+            _ = try generic_writer.write(std.mem.asBytes(&key));
+            _ = try generic_writer.write(std.mem.asBytes(&record));
+
+            //Flush
+            try heap_file_writer.flush();
+        }
+
+        // pub fn getValueByKey(lse: *Self, key: KeyType) !RecorcType {
+        //     const key_value_pair_size = @sizeOf(KeyType) + @sizeOf(RecorcType);
+        //     const data_directory = try open_dir_abs_or_cwd(io, path: []const u8)
+        //
+        // }
     };
 }
 
-test "store data" {
+test "load single record to file" {
     const io = std.testing.io;
-    const data_path = "testing/";
-    const LSE = LinearStorageEngine(.{ .data_directory = data_path, .io = io });
-    const data_dir = try std.Io.Dir.cwd().openDir(io, data_path, .{});
-    _ = data_dir.deleteFile(io, "heap.db") catch {};
-    try LSE.addData("key", "Totally legit data");
-    const heap_file = try data_dir.openFile(io, "heap.db", .{ .mode = .read_only });
-    defer heap_file.close(io);
+    const data_path = "testing";
+    const heap_file_name = "heap.db";
+
+    const test_dir = try open_dir_abs_or_cwd(io, data_path);
+    test_dir.deleteFile(io, heap_file_name) catch {}; // Clear if test ran before
+
+    var lse = try LinearStorageEngine(u64, u64).init(std.testing.allocator, .{ .data_path = data_path, .io = io, .heap_file_name = heap_file_name, .buffer_size = 1024 });
+    defer lse.deinit(std.testing.allocator);
+    try lse.storeData(1001, 2002);
+
+    const heap_file = try test_dir.openFile(io, heap_file_name, .{ .mode = .read_only });
     const heap_file_stat = try heap_file.stat(io);
-    std.debug.print("heap file size: {d}\n", .{heap_file_stat.size});
     try std.testing.expect(heap_file_stat.size > 0);
+}
+test "load multiple records to file" {
+    const io = std.testing.io;
+    const data_path = "testing";
+    const heap_file_name = "heap.db";
+
+    const test_dir = try open_dir_abs_or_cwd(io, data_path);
+    test_dir.deleteFile(io, heap_file_name) catch {}; // Clear if test ran before
+
+    var lse = try LinearStorageEngine(u64, u64).init(std.testing.allocator, .{ .data_path = data_path, .io = io, .heap_file_name = heap_file_name, .buffer_size = 1024 });
+    defer lse.deinit(std.testing.allocator);
+    try lse.storeData(1001, 2002);
+
+    const heap_file = try test_dir.openFile(io, heap_file_name, .{ .mode = .read_only });
+    defer heap_file.close(io);
+    var heap_file_stat = try heap_file.stat(io);
+    const single_record_file_size = heap_file_stat.size;
+    try lse.storeData(1001, 2002);
+    heap_file_stat = try heap_file.stat(io);
+    std.debug.print("single record size: {d}\ntwo record size: {d}\n", .{ single_record_file_size, heap_file_stat.size });
+    try std.testing.expect(heap_file_stat.size == 2 * single_record_file_size);
 }
